f41e9ea477eb1dd7e2febcc59b6aa24a4c1ed88c
Malik Shalash <mjshalash@rci4u.com>
2019-06-25 21:40:21 -0400

    Restructure files from template

--- src/components/common/Template.js
+++ src/components/common/Template.js
@@ -3,15 +3,15 @@ import Navbar from "../Navbar";
 import Package from "../../../package.json";
 const { name } = Package;
 const Template = ({ title, className, children }) => (
-  <span id='template'>
+  <span id="template">
     <Head>
       <title>{title || name}</title>
       <meta
-        name='viewport'
-        content='initial-scale=1.0, width=device-width'
-        key='viewport'
+        name="viewport"
+        content="initial-scale=1.0, width=device-width"
+        key="viewport"
       />
-      <link rel='stylesheet' href='./static/bootstrap.min.css' />
+      <link rel="stylesheet" href="./static/css/bootstrap.min.css" />
     </Head>
     <Navbar siteName={"Moi-Next"} />
     <br />
