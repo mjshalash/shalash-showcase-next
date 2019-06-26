3183fc0e2e81bf722e5d8fd4fe3fa088db2bd449
Malik Shalash <mjshalash@rci4u.com>
2019-06-25 21:29:24 -0400

    Modified Index page, added template files, added logo

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
-import { Template} from '../src/components/common'
-import HelloWorld from '../src/components/HelloWorld'
+import { Template } from "../src/components/common";
+import HelloWorld from "../src/components/HelloWorld";
 const Index = () => (
-    <Template title={'Moi Next'} className={"container mx-auto mt-5 h-100"}>
-        <HelloWorld>
-            <img className="mx-auto" style={{width:'50px'}} src="/static/moi.png" alt="my image" />
-            <h1>Hello World!</h1>
-            <p className="lead">Welcome to Moi-Next</p>    
-        </HelloWorld>
-    </Template>
+  <Template
+    title={"Shalash Showcase"}
+    className={"container mx-auto mt-5 h-100"}
+  >
+    <HelloWorld>
+      <img
+        className="mx-auto"
+        style={{ width: "50px" }}
+        src="/static/img/MalikLogo2.jpg"
+        alt="my image"
+      />
+      <h1>Hello World!</h1>
+      <p className="lead">Welcome to Shalash Showcase</p>
+    </HelloWorld>
+  </Template>
 );
 
-export default Index
+export default Index;
