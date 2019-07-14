0bd238254dfe811b689d3a09d89dea1bd6c8c103
Malik Shalash <mjshalash@rci4u.com>
2019-07-13 23:21:25 -0400

    Added global path so sidebar remains fixed and does not overlay page content
    Eventually want to modify this so it is incorporated into the template or some other higher order component to seperate the navbar from the page content
    
    --Maybe could make the navbar have a "child" prop

--- pages/projects.js
+++ pages/projects.js
@@ -12,56 +12,63 @@ const Projects = () => {
           <Navbar siteName={"Shalash Showcase"} />
         </div>
 
-        {/* Project Gallery */}
-        <div className="d-flex flex-column w-100">
-          {/* Grid Heading */}
-          <div className="d-flex flex-row justify-content-center pt-3">
-            <h4>Projects</h4>
-          </div>
-
+        {/* Projects */}
+        <div className="css-page-content">
           {/* Project Gallery */}
-          <div className="d-flex flex-column align-items-center">
-            <div className="d-flex flex-row mb-4">
-              <Project
-                img={"../static/img/Project-Gallery/placeholder-image.png"}
-                title={"Project 1"}
-              />
-              <Project
-                img={"../static/img/Project-Gallery/placeholder-image.png"}
-                title={"Project 2"}
-              />
-              <Project
-                img={"../static/img/Project-Gallery/placeholder-image.png"}
-                title={"Project 3"}
-              />
+          <div className="d-flex flex-column w-100">
+            {/* Grid Heading */}
+            <div className="d-flex flex-row justify-content-center pt-3">
+              <h4>Projects</h4>
             </div>
-            <div className="d-flex flex-row mb-4">
-              <Project
-                img={"../static/img/Project-Gallery/placeholder-image.png"}
-                title={"Project 4"}
-              />
-              <Project
-                img={"../static/img/Project-Gallery/placeholder-image.png"}
-                title={"Project 5"}
-              />
-              <Project
-                img={"../static/img/Project-Gallery/placeholder-image.png"}
-                title={"Project 6"}
-              />
-            </div>
-            <div className="d-flex flex-row mb-4">
-              <Project
-                img={"../static/img/Project-Gallery/placeholder-image.png"}
-                title={"Project 7"}
-              />
-              <Project
-                img={"../static/img/Project-Gallery/placeholder-image.png"}
-                title={"Project 8"}
-              />
-              <Project
-                img={"../static/img/Project-Gallery/placeholder-image.png"}
-                title={"Project 9"}
-              />
+
+            <div className="d-flex flex-column align-items-center">
+              {/* Row 1 */}
+              <div className="d-flex flex-row mb-4">
+                <Project
+                  img={"../static/img/Project-Gallery/placeholder-image.png"}
+                  title={"Project 1"}
+                />
+                <Project
+                  img={"../static/img/Project-Gallery/placeholder-image.png"}
+                  title={"Project 2"}
+                />
+                <Project
+                  img={"../static/img/Project-Gallery/placeholder-image.png"}
+                  title={"Project 3"}
+                />
+              </div>
+
+              {/* Row 2 */}
+              <div className="d-flex flex-row mb-4">
+                <Project
+                  img={"../static/img/Project-Gallery/placeholder-image.png"}
+                  title={"Project 4"}
+                />
+                <Project
+                  img={"../static/img/Project-Gallery/placeholder-image.png"}
+                  title={"Project 5"}
+                />
+                <Project
+                  img={"../static/img/Project-Gallery/placeholder-image.png"}
+                  title={"Project 6"}
+                />
+              </div>
+
+              {/* Row 3 */}
+              <div className="d-flex flex-row mb-4">
+                <Project
+                  img={"../static/img/Project-Gallery/placeholder-image.png"}
+                  title={"Project 7"}
+                />
+                <Project
+                  img={"../static/img/Project-Gallery/placeholder-image.png"}
+                  title={"Project 8"}
+                />
+                <Project
+                  img={"../static/img/Project-Gallery/placeholder-image.png"}
+                  title={"Project 9"}
+                />
+              </div>
             </div>
           </div>
         </div>
